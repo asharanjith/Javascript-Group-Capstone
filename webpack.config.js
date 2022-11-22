bc128c57b6307af00f0789e68f29983d15c6b521
Trust7 <simeontrust7@gmail.com>
2022-11-22 14:16:24 +0100

    Add styling to the movies page

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,21 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        include: path.resolve(
+          __dirname,
+          './node_modules/bootstrap-icons/font/fonts',
+        ),
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'webfonts',
+            publicPath: '../webfonts',
+          },
+        },
+      },
     ],
   },
 };
