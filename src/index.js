3dcb80ded6f9781aab3271be1259f18deec69314
Trust7 <simeontrust7@gmail.com>
2022-11-22 09:40:31 +0100

    Add code to fetch and display movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './style.css';
+import getMovies from "./modules/getMovies";
+import "./style.css";
+
+const movieList = document.getElementById("movieList");
+const getScores = (movieList) => {
+  movieList.replaceChildren();
+  getMovies().then((res) => {
+    const myMovies = res.slice(0, 8);
+    myMovies.map((movie) => {
+      const movieCard = document.createElement("div");
+      const movieImage = document.createElement("img");
+      movieImage.src = movie.image.medium;
+      movieCard.appendChild(movieImage);
+      const commentBtn = document.createElement("button");
+      commentBtn.innerHTML = "comments";
+      movieCard.appendChild(commentBtn);
+      movieList.appendChild(movieCard);
+      return "";
+    });
+  });
+};
+
+getScores(movieList);
---
+++ src/modules/getMovies.js
@@ -0,0 +1,9 @@
+const url = "https://api.tvmaze.com/shows";
+
+const getMovies = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+export default getMovies;
