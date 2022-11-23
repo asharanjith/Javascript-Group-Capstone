a0997414d503b4701ebe785e61ed436256284f3b
Trust7 <simeontrust7@gmail.com>
2022-11-23 11:22:51 +0100

    Add code to display number of movies in the navigation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import displayMovies from './modules/getMovies.js';
+import { displayMovies, getMovies } from './modules/getMovies.js';
+import movieCounter from './modules/movieCounter.js';
 import './style.css';
 
 const movieList = document.getElementById('movieList');
 displayMovies(movieList);
+
+const movieCount = document.getElementById('movie-counter');
+getMovies().then((data) => {
+  movieCounter(data, movieCount);
+});
